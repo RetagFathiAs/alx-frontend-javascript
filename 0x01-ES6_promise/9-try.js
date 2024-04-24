c7ae421f2c230c5bbcd6e55558488e3ae755a6a7
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-04-25 01:49:51 +0200

    Update 9-try.js

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,9 +1,13 @@
 export default function guardrail(mathFunction) {
-    const queue = [];
-    try {
-      queue.push(mathFunction(), 'Guardrail was processed');
-    } catch (err) {
-      queue.push(`Error: ${err.message}`, 'Guardrail was processed');
-    }
-    return queue;
+  const queue = [];
+  let res;
+  try {
+    res = mathFunction();
+  } catch (error) {
+    res = `Error: ${error.message}`;
   }
+  queue.push(res);
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
