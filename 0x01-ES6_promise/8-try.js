604bb721aaa0136a48be22a4a37330edbcf8845e
RetagFathiAs <136014374+RetagFathiAs@users.noreply.github.com>
2024-04-25 01:49:09 +0200

    alx

--- 0x01-ES6_promise/8-try.js
+++ 0x01-ES6_promise/8-try.js
@@ -1,7 +1,10 @@
 export default function divideFunction(numerator, denominator) {
+  try {
     if (denominator === 0) {
-      throw Error('cannot divide by 0');
-    } else {
-      return numerator / denominator;
+      throw new Error();
     }
+    return numerator / denominator;
+  } catch (error) {
+    throw Error('cannot divide by 0');
   }
+}
